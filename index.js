afcea255deeb07f9de517ecaa643767bd6129bca
Mohit Shende <mohitshende023@gmail.com>
2021-09-29 19:19:09 +0530

    cors changes

--- index.js
+++ index.js
@@ -4,10 +4,33 @@ connectToMongo();
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const cors = require("cors");
 
-app.use(cors());
+const frontend_url = process.env.FRONTEND;
+
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:8080",
+  frontend_url,
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable");
+      callback(null, true);
+    } else {
+      console.log("Origin rejected");
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+// Add react app frontend url later on
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
